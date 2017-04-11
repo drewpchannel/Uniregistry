7b5909fd3e71b81b1e9bac2bc0dce6782facd9b2
drewpchannel <drewpcchannel@gmail.com>
2017-04-11 14:45:32 -0500

    creates new table from edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import domains from '../assets/domains.json'
 
 class DomainTable extends Component {
   constructor(props){
     super(props);
     this.state = {
-      currentTable: []
+      currentTable: [],
+      domains: {},
+      editorForm: {}
     }
   }
 
@@ -16,12 +19,11 @@ class DomainTable extends Component {
   }
 
   createTableArray() {
-    var domains = require('../assets/domains.json');
     var tableArray = [];
     domains.domains.forEach((elem, index) => {
       tableArray.push(
         <tr key={domains.domains[index].domain}>
-          <td><a href="#" onClick={this.loadEditor}>{domains.domains[index].domain}</a></td>
+          <td><a href="#" onClick={this.loadEditor.bind(this)}>{domains.domains[index].domain}</a></td>
           <td>{this.displayCheck(domains.domains[index].id)}</td>
           <td>${(domains.domains[index].price/100).toFixed(2)}</td>
         </tr>
@@ -32,16 +34,36 @@ class DomainTable extends Component {
 
   componentWillMount() {
     this.createTableArray();
+    this.setState({domains: domains})
   }
 
-  loadEditor(){
+  loadEditor(linkClicked){
     $('#mainTable').hide();
     $('#editor').show();
+    var foundData = [];
+    domains.domains.forEach((elem, index) => {
+      if (elem.domain === linkClicked.target.text) {
+        foundData = domains.domains[index];
+      }
+    });
+    this.setState({editorForm: foundData})
   }
 
   saveButton() {
     $('#mainTable').show();
     $('#editor').hide();
+    var tableChanges = this.state.domains.domains;
+    tableChanges.forEach((elem, index) => {
+      if (tableChanges[index].domain === $('#domainName')[0].placeholder) {
+        tableChanges.splice(index, 1);
+        tableChanges.push({
+          domain: $('#domainName')[0].value,
+          email: $('#registranEmail')[0].value,
+          price: $('#priceValue')[0].value
+        })
+      
+      }
+    });
   }
 
   render() {
@@ -60,7 +82,33 @@ class DomainTable extends Component {
           </tbody>
         </table>
         <div id="editor" style={{display:'none'}}>
-          <text onClick={this.saveButton}>editor</text>
+          
+          <form className="form-horizontal" style={{maxWidth:'600px'}}>
+            <div className="form-group">
+              <label className="control-label col-sm-2">Domain Name:</label>
+              <div className="col-sm-10">
+                <input className="form-control" id="domainName" placeholder={this.state.editorForm.domain}></input>
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="control-label col-sm-2">Registran Email:</label>
+              <div className="col-sm-10"> 
+                <input className="form-control" id="registranEmail" placeholder="Enter email"></input>
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="control-label col-sm-2">Price:</label>
+              <div className="col-sm-10"> 
+                <input className="form-control" id="priceValue" placeholder={this.state.editorForm.price}></input>
+              </div>
+            </div>
+            <div className="form-group"> 
+              <div className="col-sm-offset-2 col-sm-10">
+                <button type="submit" className="btn btn-default" onClick={this.saveButton.bind(this)}>Save Changes</button>
+              </div>
+            </div>
+          </form>
+
         </div>
       </div>
     );
